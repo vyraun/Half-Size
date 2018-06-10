1c30a94e80e675f5ee9ca55c76719746090f3836
Vikas Raunak <vyraun@gmail.com>
2018-06-10 14:08:40 +0530

    Create reuters52.py

---
+++ reuters52.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+X, y = [], []
+with open("downstream_datasets/r52-train-no-stop.txt", "r") as infile:
+    for line in infile:
+        label, text = line.split("\t")
+        # texts are already tokenized, just split on space
+        # in a real case we would use e.g. spaCy for tokenization
+        # and maybe remove stopwords etc.
+        X.append(text.split())
+        y.append(label)
+X, y = np.array(X), np.array(y)
+print ("total examples %s" % len(y))
+
+with open("glove.6B.200d.txt", "rb") as lines:
+    w2v = {line.split()[0]: np.array(map(float, line.split()[1:]))
+           for line in lines}
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import Counter, defaultdict
+
+class MeanEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        # if a text is empty we should return a vector of zeros
+        # with the same dimensionality as all the other vectors
+        self.dim = len(next(iter(word2vec)))
+
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X):
+        return np.array([
+            np.mean([self.word2vec[w] for w in words if w in self.word2vec]
+                    or [np.zeros(self.dim)], axis=0)
+            for words in X
+        ])
+
+class TfidfEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        self.word2weight = None
+        self.dim = len(next(iter(word2vec)))
+
+    def fit(self, X, y):
+        tfidf = TfidfVectorizer(analyzer=lambda x: x)
+        tfidf.fit(X)
+        # if a word was never seen - it must be at least as infrequent
+        # as any of the known words - so the default idf is the max of 
+        # known idf's
+        max_idf = max(tfidf.idf_)
+        self.word2weight = defaultdict(
+            lambda: max_idf,
+            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
+
+        return self
+
+    def transform(self, X):
+        return np.array([
+                np.mean([self.word2vec[w] * self.word2weight[w]
+                         for w in words if w in self.word2vec] or
+                        [np.zeros(self.dim)], axis=0)
+                for words in X
+            ])
+
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import ExtraTreesClassifier
+
+etree_w2v = Pipeline([
+    ("word2vec vectorizer", MeanEmbeddingVectorizer(w2v)),
+    ("extra trees", ExtraTreesClassifier(n_estimators=200))])
+etree_w2v_tfidf = Pipeline([
+    ("word2vec vectorizer", TfidfEmbeddingVectorizer(w2v)),
+    ("extra trees", ExtraTreesClassifier(n_estimators=200))])
+
+all_models = [
+    ("etree_w2v", etree_w2v),
+    ("etree_w2v_tfidf", etree_w2v_tfidf)
+]
+
+
+import tabulate
+from tabulate import tabulate
+
+from sklearn.cross_validation import cross_val_score
+unsorted_scores = [(name, cross_val_score(model, X, y, cv=5).mean()) for name, model in all_models]
+scores = sorted(unsorted_scores, key=lambda x: -x[1])
+
+
+print (tabulate(scores, floatfmt=".4f", headers=("model", 'score')))
